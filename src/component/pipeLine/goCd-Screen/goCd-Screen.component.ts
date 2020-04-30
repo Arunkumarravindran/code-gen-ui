c2adbb6131e272d75e0164dc9ee68afc1a81d234
user <arunkumarravindran@outlook.com>
2020-04-30 21:24:12 +0530

    db and pipeline layout done

---
+++ src/component/pipeLine/goCd-Screen/goCd-Screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goCd-Screen',
+  templateUrl: './goCd-Screen.component.html',
+  styleUrls: ['./goCd-Screen.component.css']
+})
+export class GoCdScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/component/pipeLine/jenkins-Screen/jenkins-Screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jenkins-Screen',
+  templateUrl: './jenkins-Screen.component.html',
+  styleUrls: ['./jenkins-Screen.component.css']
+})
+export class JenkinsScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
